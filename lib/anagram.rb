6c6d83b7b042a6293102927b82773ee9408a2517
Martin <mkmartinoes@gmail.com>
2022-06-09 15:38:37 +0300

    Complitted Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    @array = array
+    new_word = word_sort(@word)
+    p word = @array.filter {
+      |item| word_sort(item) == new_word
+    }
+  end
+
+  def word_sort word
+    word.split("").sort
+  end
+
+
+end
+
+# pp = Anagram.new("word")
+
+# pp.match(["Lucy", "word", "wrod"])
+
+
